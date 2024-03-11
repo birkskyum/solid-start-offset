e6c45b06c91b05dfeb8c3111bbe7f2224bae3f50
Birk Skyum <birk.skyum@pm.me>
2024-03-11 23:44:02 +0100

    add slash

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   ssr: true,
   server: {
-    baseURL: '/solid-start-offset',
+    baseURL: '/solid-start-offset/',
     preset: 'github-pages'
   }
 });
